70924f5611c75e0e85ff0bee3c10a900ef07210e
arma73 <arma.biz9805@gmail.com>
2022-02-28 00:13:41 +0400

    feat(@amollo-lint:eslint-jsx): rules for react jsx

---
+++ packages/@eslint/eslint-config-jsx/index.js
@@ -0,0 +1,46 @@
+module.exports = {
+    "env": {
+        "jest": true,
+    },
+    "extends": [
+        "@amollo-lint/eslint-config-js",
+        "plugin:react/recommended", // eslint-plugin-react
+        "plugin:jsx-a11y/recommended", // eslint-plugin-jsx-a11y
+    ],
+    "plugins": ["react", "react-hooks", "jsx-a11y"],
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    "overrides": [
+        {
+            "files": ["*.jsx"],
+            "ecmaFeatures": {
+                "jsx": true, // Allows for the parsing of JSX
+            }
+        }
+    ],
+    "rules": {
+        "jsx-quotes": [2, "prefer-double"],
+        "react/react-in-jsx-scope": "off",
+        "jsx-a11y/anchor-is-valid": [
+            2,
+            {
+                "aspects": ["invalidHref", "preferButton"],
+                "components": ["Link"],
+                "specialLink": ["route"],
+            },
+        ],
+        "react/prop-types": [
+			"error",
+			{
+				"skipUndeclared": true,
+			},
+		],
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "error",
+		"no-param-reassign": "warn",
+		"consistent-return": "warn",
+    }
+};
