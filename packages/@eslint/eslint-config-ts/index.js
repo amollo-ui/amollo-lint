be4590f1913ceb61563dd47cda969cb53dfc415b
arma73 <arma.biz9805@gmail.com>
2022-03-02 01:32:56 +0400

    feat(@amollo-lint:eslint-ts): overrides for ext ts and tsx

--- packages/@eslint/eslint-config-ts/index.js
+++ packages/@eslint/eslint-config-ts/index.js
@@ -2,32 +2,16 @@ const rules = require("./rules");
 
 module.exports = {
     "root": true,
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "es2017": true,
-        "es2020": true,
-        "node": true,
-    },
-    "extends": [
-        "@amollo-lint/eslint-config-js",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-    ],
-    "plugins": ["@typescript-eslint"],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module",
-	},
+    "extends": ["@amollo-lint/eslint-config-js"],
     "overrides": [
         {
-            "files": ["*.ts"],
-            "parserOptions": {
-                "project": "tsconfig.json",
-                "tsconfigRootDir": __dirname,
-            },
+            "files": ["*.{ts,tsx}"],
+            "extends": [
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended",
+            ],
+            "parser": "@typescript-eslint/parser",
+            "plugins": ["@typescript-eslint"],
             rules
         }
     ]
